b0b5ad69210949caf55f25c63ae3229210f9e6ec
Qian <13339502@qq.com>
2019-04-23 18:24:33 +0900

    chabnge

--- MyHappyMall/src/main/java/com/mmall/service/IProductService.java
+++
@@ -1,9 +0,0 @@
-package com.mmall.service;
-
-import com.mmall.commons.ServerResponse;
-import com.mmall.vo.ProductDetailVo;
-
-public interface IProductService {
-
-    public ServerResponse<ProductDetailVo> getDetailInfo(Integer productId);
-}
